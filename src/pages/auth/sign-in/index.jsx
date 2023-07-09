ae5144f8c5a1b72d7f604d50b2b449997dd991c8
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-07-09 11:13:01 +0300

    feat: add sign-in page

--- src/pages/auth/sign-in/index.jsx
+++ src/pages/auth/sign-in/index.jsx
@@ -1,13 +1,16 @@
 import { observer } from "mobx-react-lite";
 import { Navigate } from "react-router-dom";
 
-import cn from "classnames";
-
-// import { Input } from "@/shared/ui/Input";
-// import { Button } from "@/shared/ui/Button";
+import { Input } from "../../../shared/ui/Input";
+import { Button } from "../../../shared/ui/Button";
 
 import styles from "./styles.module.css";
 import { useModel } from "./model";
+
+import logo from "./logo.svg";
+import googleLogo from "./logo-google.svg";
+import yandexLogo from "./logo-yandex.svg";
+import weiboLogo from "./logo-weibo.svg";
 
 const SignInPage = () => {
   const model = useModel();
@@ -15,8 +18,76 @@ const SignInPage = () => {
   return (
     <>
       <header className={styles.header}>
+          <img src={logo} alt="Логотип проекта"/>
+          <Button
+              type="submit"
+              variant="primary"
+              size="s"
+              disabled={model.isLoading}
+          >
+              {model.isLoading ? "Loading" : "Регистрация"}
+          </Button>
       </header>
       <main className={styles.content}>
+          <h1 className={styles.title}>Войти через</h1>
+          <Button
+              className={styles.button}
+              type="submit"
+              variant="white"
+              size="l"
+              disabled={model.isLoading}
+          >
+              <img className={styles.logo} src={googleLogo} alt="Логотип гугл"/>
+              {model.isLoading ? "Loading" : "Вход через аккаунт Google"}
+          </Button>
+          <Button
+              className={styles.button}
+              type="submit"
+              variant="white"
+              size="l"
+              disabled={model.isLoading}
+          >
+              <img className={styles.logo} src={yandexLogo} alt="Логотип гугл"/>
+              {model.isLoading ? "Loading" : "Вход через аккаунт Yandex"}
+          </Button>
+          <Button
+              className={styles.button}
+              type="submit"
+              variant="white"
+              size="l"
+              disabled={model.isLoading}
+          >
+              <img className={styles.logo} src={weiboLogo} alt="Логотип гугл"/>
+              {model.isLoading ? "Loading" : "Вход через аккаунт Weibo"}
+          </Button>
+          <p className={styles.text}>или</p>
+          <Input
+              type="text"
+              name="email"
+              value={model.email}
+              placeholder="Email или номер телефона"
+              required
+              onValue={model.handleChange}
+          />
+          <Input
+              className={styles.input}
+              type="password"
+              name="password"
+              value={model.password}
+              placeholder="Пароль"
+              required
+              onValue={model.handleChange}
+          />
+          <Button
+              className={styles.button}
+              type="submit"
+              variant="primary"
+              size="m"
+              disabled={model.isLoading}
+          >
+              {model.isLoading ? "Loading" : "Войти"}
+          </Button>
+          <a className={styles.link} href="#">Не можешь вспомнить свой пароль?</a>
       </main>
     </>
   );
