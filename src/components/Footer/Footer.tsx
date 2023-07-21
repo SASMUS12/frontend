ca11055453d033fa7c6ad9fb4c424effa5925647
Evgenia Kurakina <101995565+krokodila888@users.noreply.github.com>
2023-07-21 21:33:20 +0300

    FEAT/FIX and dependencies to add style modules
    Add components and dependencies to add style modules

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import styles from "./styles.module.css";
+import { NavLink, useLocation, Link, useNavigate } from 'react-router-dom';
+
+function Footer() {
+
+  return (
+    <footer className='footer'>
+      
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import styles from "./Header.module.css";
+import { NavLink, useLocation, Link, useNavigate } from 'react-router-dom';
+
+function Header() {
+
+  return (
+    <header className={styles.footer}>
+      
+    </header>
+  );
+}
+
+export default Header;
