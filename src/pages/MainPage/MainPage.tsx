16c038b741c86a802529a65fa350e4bf257a551b
Evgenia Kurakina <101995565+krokodila888@users.noreply.github.com>
2023-07-24 22:35:20 +0300

    FEAT: add Header and Footer to MainPage.tsx

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,27 +1,35 @@
 import { Card } from "../../components/Card/Card";
 import { results } from "./Cards";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 import styles from "./MainPage.module.scss";
 
 const MainPage = () => {
   return (
-      <main className={styles.content}>
-          <section className={styles.content__cardList}>
-              {results.map((partner) => (
-                  <Card
-                      country={partner.country}
-                      status={partner.status}
-                      first_name={partner.first_name}
-                      gender={partner.gender}
-                      age={partner.age}
-                      about={partner.about}
-                      indicator={partner.indicator}
-                      nativeLanguages={partner.native_languages}
-                      foreignLanguages={partner.foreign_languages}
-                  />
-              ))}
-          </section>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.content__cardList}>
+            {results.map((partner) => (
+              <Card
+                country={partner.country}
+                status={partner.status}
+                first_name={partner.first_name}
+                gender={partner.gender}
+                age={partner.age}
+                about={partner.about}
+                indicator={partner.indicator}
+                nativeLanguages={partner.native_languages}
+                foreignLanguages={partner.foreign_languages}
+              />
+            ))}
+          </div>
+        </section>
       </main>
+      <Footer />
+    </>
   );
 };
 
