ddebbc02475c17d9f896eceeda6967639f72a929
Evgenia Kurakina <101995565+krokodila888@users.noreply.github.com>
2023-07-24 22:15:33 +0300

    FEAT: change Button styles

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,11 +1,10 @@
 import cn from 'classnames';
-
 import styles from './Button.module.scss';
 
-export const Button = ({ className, type, variant, size, disabled, onClick, children }) => {
+export const Button = ({ className, type, variant, disabled, onClick, children }) => {
   return (
     <button
-      className={cn(styles.root, styles[`size-${size}`], styles[`variant-${variant}`], className)}
+      className={cn(styles.root, styles[`variant-${variant}`], className)}
       type={type}
       disabled={disabled}
       onClick={onClick}>
