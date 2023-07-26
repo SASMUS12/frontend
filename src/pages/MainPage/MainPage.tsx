4bf2d59857583086b905f2ae071429657c2fadc0
Мария Березина <BerezinaMariya.spb@yandex.ru>
2023-07-26 22:28:29 +0300

    fix: type of usersList in mainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,9 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState } from "react";
 
-import { Api } from "../../utils/openapi";
-import { baseUrl } from "../../utils/constants"
+import {Api} from "../../utils/openapi";
+import {baseUrl} from "../../utils/constants"
 
-import { Card } from "../../components/Card/Card";
+import {Card} from "../../components/Card/Card";
 
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
@@ -11,54 +11,56 @@ import Footer from "../../components/Footer/Footer";
 import styles from "./MainPage.module.scss";
 
 const MainPage = () => {
+    const api = new Api({baseUrl: baseUrl});
 
-    const [usersList, setUsersList] = useState([]);
-        const getUsersList = async () => {
-            try {
-                const api = new Api({baseUrl: baseUrl});
-                console.log('отправка запроса ---');
-                const response = await api.api.usersList({
-                    limit: 10,
-                });
-                console.log('ответ получен -', response);
+    const [usersList, setUsersList] = useState<any[]>([]);
+    const [isUsersList, setIsUsersList] = useState(false);
+    const getUsersList = async () => {
+        try {
+            console.log('отправка запроса ---');
+            const response = await api.api.usersList();
+            console.log('ответ получен -', response);
+            setIsUsersList(true);
 
-                if (response.data && response.data.results) {
-                    setUsersList(response.data.results);
-                }
-            } catch (error) {
-                console.error('Ошибка при получении данных -', error);
+            if (response.data && response.data.results) {
+                setUsersList(response.data.results);
             }
-        };
+        } catch (error) {
+            console.error('Ошибка при получении данных -', error);
+            setIsUsersList(false);
+        }
+    };
 
     useEffect(() => {
         getUsersList();
     }, []);
 
-  return (
-    <>
-      <Header />
-      {/*<main className={styles.main}>*/}
-      {/*  <section className={styles.content}>*/}
-      {/*    <div className={styles.content__cardList}>*/}
-      {/*      {usersList.map((user) => (*/}
-      {/*        <Card*/}
-      {/*          country={user.country}*/}
-      {/*          status={user.status}*/}
-      {/*          first_name={user.first_name}*/}
-      {/*          gender={user.gender}*/}
-      {/*          age={user.age}*/}
-      {/*          about={user.about}*/}
-      {/*          indicator={user.indicator}*/}
-      {/*          nativeLanguages={user.native_languages}*/}
-      {/*          foreignLanguages={user.foreign_languages}*/}
-      {/*        />*/}
-      {/*      ))}*/}
-      {/*    </div>*/}
-      {/*  </section>*/}
-      {/*</main>*/}
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.content__cardList}>
+                        {isUsersList &&
+                            usersList.map((partner) => (
+                                <Card
+                                    country={partner.country}
+                                    status={partner.status}
+                                    first_name={partner.first_name}
+                                    gender={partner.gender}
+                                    age={partner.age}
+                                    about={partner.about}
+                                    indicator={partner.indicator}
+                                    nativeLanguages={partner.native_languages}
+                                    foreignLanguages={partner.foreign_languages}
+                                />
+                            ))}
+                    </div>
+                </section>
+            </main>
+            <Footer/>
+        </>
+    );
 };
 
 export default MainPage;
