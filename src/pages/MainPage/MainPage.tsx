e056e1976f75c08bb7be909ef1af54127ce70fc3
Мария Березина <BerezinaMariya.spb@yandex.ru>
2023-07-26 21:51:12 +0300

    feat: get usersList from Api

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,27 +1,63 @@
+import {useEffect, useState} from "react";
+
+import { Api } from "../../utils/openapi";
+import { baseUrl } from "../../utils/constants"
+
 import { Card } from "../../components/Card/Card";
-import { results } from "./Cards";
+
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 import styles from "./MainPage.module.scss";
 
 const MainPage = () => {
+
+    const [usersList, setUsersList] = useState([]);
+        const getUsersList = async () => {
+            try {
+                const api = new Api({baseUrl: baseUrl});
+                console.log('отправка запроса ---');
+                const response = await api.api.usersList({
+                    limit: 10,
+                });
+                console.log('ответ получен -', response);
+
+                if (response.data && response.data.results) {
+                    setUsersList(response.data.results);
+                }
+            } catch (error) {
+                console.error('Ошибка при получении данных -', error);
+            }
+        };
+
+    useEffect(() => {
+        getUsersList();
+    }, []);
+
   return (
-      <main className={styles.content}>
-          <section className={styles.content__cardList}>
-              {results.map((partner) => (
-                  <Card
-                      country={partner.country}
-                      status={partner.status}
-                      first_name={partner.first_name}
-                      gender={partner.gender}
-                      age={partner.age}
-                      about={partner.about}
-                      indicator={partner.indicator}
-                      nativeLanguages={partner.native_languages}
-                      foreignLanguages={partner.foreign_languages}
-                  />
-              ))}
-          </section>
-      </main>
+    <>
+      <Header />
+      {/*<main className={styles.main}>*/}
+      {/*  <section className={styles.content}>*/}
+      {/*    <div className={styles.content__cardList}>*/}
+      {/*      {usersList.map((user) => (*/}
+      {/*        <Card*/}
+      {/*          country={user.country}*/}
+      {/*          status={user.status}*/}
+      {/*          first_name={user.first_name}*/}
+      {/*          gender={user.gender}*/}
+      {/*          age={user.age}*/}
+      {/*          about={user.about}*/}
+      {/*          indicator={user.indicator}*/}
+      {/*          nativeLanguages={user.native_languages}*/}
+      {/*          foreignLanguages={user.foreign_languages}*/}
+      {/*        />*/}
+      {/*      ))}*/}
+      {/*    </div>*/}
+      {/*  </section>*/}
+      {/*</main>*/}
+      <Footer />
+    </>
   );
 };
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const bazeUrl = 'http://5.181.255.32/api/v1/';
+export const baseUrl = 'http://5.181.255.32';
