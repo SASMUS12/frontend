6866f3b3f33f032b4b0db6a705ece669096f23c7
Александр <avs2003@bk.ru>
2023-07-31 02:32:20 +0300

    Начал делать slider(ползунок)

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -14,6 +14,7 @@ interface SliderComponentProps {
 
 
 const Sort: React.FC<SliderComponentProps> = () => {
+
   const [sliderValue, setSliderValue] = useState<number[]>([18, 100]);
 
 
@@ -42,75 +43,65 @@ const Sort: React.FC<SliderComponentProps> = () => {
         setSliderValue([sliderValue[0], value]);
       }
     };
+
     return (
-        <div className={ styles.popup__sort}>
-            <div className={styles.popup__cantry}>
-              <h2 >Страна партнера</h2>
-              <div className={styles.popup__enter}>
-                <input
-                  type="text"
-                  id="searchInput"
-                  placeholder="Начните вводить название"
-                  // onChange={handleSearchInputChange}
-                />
-              </div>
-            </div>
-            <h2>Язык партнера</h2>
-            <LanguageLevelp/>
-            <div >
-              <Button 
-                className={styles.popup__add}
-                children={"добавить язык"}
+      <div className={ styles.popup__sort}>
+        <div className={styles.popup__cantry}>
+          <h2 >Страна партнера</h2>
+          <div className={styles.popup__enter}>
+            <input
+              type="text"
+              id="searchInput"
+              placeholder="Начните вводить название"
+              // onChange={handleSearchInputChange}
+            />
+          </div>
+          </div>
+          <h2>Язык партнера</h2>
+          <LanguageLevelp/>
+          <div>
+            <Button 
+              className={styles.popup__add}
+              children={"добавить язык"}
+            />
+          </div>
+          <div className={styles.popup__partner}>
+            <h2>О партнере</h2>
+            <div className={styles.popup__gender}>
+              <h3>Пол</h3>
+              <Button                   
+                children={"Мужчина"} 
               />
-            </div>
-            <div className={styles.popup__partner}>
-              <h2>О партнере</h2>
-              <div className={styles.popup__gender}>
-                <h3>Пол</h3>
-                <Button                   
-                  children={"Мужчина"} 
-                />
-                <Button
-                  children={"Женщина"} 
-                />
-              </div>
+              <Button
+                children={"Женщина"} 
+              />
+             </div>
             </div>
             <div className={styles.popup__age}>
-        <h3>Возраст</h3>
-        <div className={styles.slider_container}>
-        <div
-            className={styles.range}
-            style={{
-              left: `${((sliderValue[0] - 18) / (100 - 18)) * 100}%`,
-              width: `${((sliderValue[1] - sliderValue[0]) / (100 - 18)) * 100}%`,
-            }}
-          />
-          <input
-            className={styles.slider}
-            type="range"
-            min="18"
-            max="100"
-            value={sliderValue[0]}
-            onChange={handleSliderChange}
-            name="from"
-          />
-          <span className={styles.range_values}>{sliderValue[0]}</span>
-          <input
-            className={styles.slider}
-            type="range"
-            min="18"
-            max="100"
-            value={sliderValue[1]}
-            onChange={handleSliderChange}
-            name="to"
-          />
-          <span className={styles.range_values}>{sliderValue[1]}</span>
+              <h3>Возраст</h3>
+
+
+              <div className={styles.slider_wrapper}>
+                <div className={styles.container}>
+                  <div className={styles.slider_track}></div>
+                  <input type="range" min={18} max={100} value={18} name="" id="slider-1" />
+                  <input type="range" min={18} max={100} value={40} name="" id="slider-2" />     
+                </div>
+                <div className={styles.slider_values}>
+                  <span id="range1">
+                    {}
+                  </span>
+                  <span id="range2">
+                    {}
+                  </span>
+                </div>                
+              </div>
+            <div>
+              <Button children={"Найти"} />
+            </div>
         </div>
       </div>
-
-            <Button children={"Найти"} />
-        </div>
-      );
-    };
+  );
+};
     
 export default Sort;
