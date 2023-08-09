98d4cea63e178deca98167184e276535bb0706f9
Александр <avs2003@bk.ru>
2023-08-09 19:39:18 +0300

    Fix?: Изменил LanguageLevel

--- src/components/LanguageLevel/LanguageLevel.tsx
+++ src/components/LanguageLevel/LanguageLevel.tsx
@@ -1,18 +1,24 @@
-import React, {useState} from "react";
-import styles from "../../components/LanguageLevel/LanguageLevel.module.scss"
-import { Language } from '../../utils/openapi';
+import React, { useState } from "react";
+import styles from "../../components/LanguageLevel/LanguageLevel.module.scss";
+import { Language, UserForeignLanguage, UserNativeLanguage } from '../../utils/openapi';
+
+enum SkillLevelEnum {
+  Newbie = "Newbie",
+  Amateur = "Amateur",
+  Profi = "Profi",
+  Expert = "Expert",
+  Guru = "Guru",
+}
 
 interface LanguageLevelProps {
   languages: Language[];
-  onAdd: (language: Language) => void;
-  onRemove?: (language: Language) => void;
+  onAdd: (language: UserForeignLanguage | UserNativeLanguage) => void;
+  onRemove?: (language: UserForeignLanguage | UserNativeLanguage) => void;
 }
 
 const LanguageLevel: React.FC<LanguageLevelProps> = ({ languages, onAdd, onRemove }) => {
-
   const [selectedLanguage, setSelectedLanguage] = useState<Language | null>(null);
   const [selectedLevels, setSelectedLevels] = useState<string[]>([]);
-  
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const languageName = e.target.value;
@@ -21,12 +27,37 @@ const LanguageLevel: React.FC<LanguageLevelProps> = ({ languages, onAdd, onRemov
   };
 
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const languageName = e.target.name;
-    const language = languages.find((lang) => lang.name === languageName);
-    if (e.target.checked && language) {
-      onAdd(language);
-    } else if (!e.target.checked && language) {
-      onRemove?.(language);
+    const skillLevel = e.target.value;
+    const languageCode = e.target.name;
+    const language = languages.find((lang) => lang.name === languageCode);
+  
+    if (language) {
+      if (skillLevel === "Native") {
+        const userLanguage: UserNativeLanguage = {
+          code: language.isocode,
+          language: language.name,
+        };
+  
+        if (e.target.checked) {
+          onAdd(userLanguage);
+          setSelectedLevels((prevLevels) => [...prevLevels, skillLevel]);
+        } else {
+          onRemove?.(userLanguage);
+          setSelectedLevels((prevLevels) => prevLevels.filter((level) => level !== skillLevel));
+        }
+      } else {
+        const userLanguage: UserForeignLanguage = {
+          code: language.isocode,
+          language: language.name,
+          skill_level: skillLevel as SkillLevelEnum,
+        };
+  
+        if (e.target.checked) {
+          onAdd(userLanguage);
+        } else {
+          onRemove?.(userLanguage);
+        }
+      }
     }
   };
 
